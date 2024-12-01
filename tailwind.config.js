5811f0bfbf5ccb73646df284eb79cad7cd2048bc
Zach Lagden <zach@zachlagden.uk>
2024-12-01 13:37:56 +0000

    🎨 Format configuration and CSS files: adjust indentation in .pre-commit-config.yml, tailwindbase.css, and tailwind.config.js for improved readability

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/templates/*.html", "./src/templates/**/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ['./src/templates/*.html', './src/templates/**/*.html'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
