25f21ee4beacf5b642ee5602dec7b0ca925f8b21
Zach Lagden <zach@zachlagden.uk>
2024-12-04 22:20:43 +0000

    🔧 Update base.html to link Tailwind CSS stylesheet and modify tailwind.config.js to include JavaScript files in content path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/templates/*.html', './src/templates/**/*.html'],
+    content: ['./src/templates/*.html', './src/templates/**/*.html', './src/static/js/*.js'],
     theme: {
         extend: {},
     },
